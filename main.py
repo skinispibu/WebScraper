679f1c4657f9c3f4b1f5380839db967bd02adc2b
skinispibu <Kimhagoong@gmail.com>
2021-08-10 23:15:27 +0900

    CYFPF

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
-# Lists in python 2021/08/09
-days = ["Mon", "Tue", "Wed", "Thur", "Fri"]
-print(days)
-days.append("Sat")
-days.reverse()
-print(days)
+# # Lists in python 2021/08/09
+# days = ["Mon", "Tue", "Wed", "Thur", "Fri"]
+# print(days)
+# days.append("Sat")
+# days.reverse()
+# print(days)
+# # Lists in python 2021/08/10
+하경 = {"name" : "김하궁",
+"age" : 17,
+"korean" : True,
+"fav_food": ["떡볶이","불닭"]}
+print(하경)
+하경["pretty"] = True 
+print(하경)
+
+age = "17"
+print(age)
+print(type(age))
+n_age = int(age)
+print(n_age)
+print(type(n_age))
+
+def nana():
+    print("오빠")
+    print("내가 한거야")
+nana()
+nana()
+nana()
+nana()
+nana()
+nana()
+nana()
+nana()
+nana()
+nana()
+
