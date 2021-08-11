f9b9b571c36d2bb92f776847e7b756fd576c7c23
skinispibu <Kimhagoong@gmail.com>
2021-08-11 17:06:57 +0900

    KC

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # #Lists in python 2021/08/09
 # # days = ["Mon", "Tue", "Wed", "Thur", "Fri"]
 # # print(days)
@@ -59,31 +58,3 @@ def nana(name, age, are_from, fav_food):
     return f"Hello {name} you are {age} years old you are from {are_from} you like {fav_food}"
 nana_2 = nana(name="하경",age="17", fav_food="bread", are_from="korea")
 print(nana_2)
-=======
-# # Lists in python 2021/08/09
-# days = ["Mon", "Tue", "Wed", "Thur", "Fri"]
-# print(days)
-# days.append("Sat")
-# days.reverse()
-# print(days)
-# # Lists in python 2021/08/10
-하경 = {"name" : "김하궁",
-"age" : 17,
-"korean" : True,
-"fav_food": ["떡볶이","불닭"]}
-print(하경)
-하경["pretty"] = True 
-print(하경)
-
-age = "17"
-print(age)
-print(type(age))
-n_age = int(age)
-print(n_age)
-print(type(n_age))
-
-def nana():
-    print("오빠")
-    print("내가 한거야")
-nana()
->>>>>>> 58ee9219549228f3b1d981e02a8ec0528f6e6ac7
