aa1a7c7994a30a52ec807df53fca9f3b1d7e07e3
skinispibu <Kimhagoong@gmail.com>
2021-08-15 21:58:02 +0900

    requests

--- main.py
+++ main.py
@@ -69,40 +69,48 @@
 # plus(3, 4.5)
 
 # List in python 2021/08/14
-def age_check(age):
-    print(f"you are {age}")
-    if age < 18:
-        print("you can't drink")
-    elif age ==18 or age ==19:
-        print("you are new to this!")
-    elif age >20 and age < 25:
-        print("you are still kind of young")
-    else:
-        print("enjoy your drink")
+# def age_check(age):
+#     print(f"you are {age}")
+#     if age < 18:
+#         print("you can't drink")
+#     elif age ==18 or age ==19:
+#         print("you are new to this!")
+#     elif age >20 and age < 25:
+#         print("you are still kind of young")
+#     else:
+#         print("enjoy your drink")
 
-age_check(17)
+# age_check(17)
 
-days = ("Mon", "Tue", "Wed", "Thu", "Fri")
+# days = ("Mon", "Tue", "Wed", "Thu", "Fri")
 
-for love in days:
-    if love is "Wed":
-        break
-    else:
-        print(love)
+# for love in days:
+#     if love is "Wed":
+#         break
+#     else:
+#         print(love)
 
-for letter in "hagyeong":
-    print(letter)
+# for letter in "hagyeong":
+#     print(letter)
 
-# Modules are math function.
-import math
+# # Modules are math function.
+# import math
 
-print(math.ceil(1.1))
-print(math.fabs(-4.5))
+# print(math.ceil(1.1))
+# print(math.fabs(-4.5))
 
-from math import fsum as love_sum
+# from math import fsum as love_sum
 
-print(love_sum([1, 2, 3, 4, 5, 6]))
+# print(love_sum([1, 2, 3, 4, 5, 6]))
 
-from caculator import plus, minus
+# from caculator import plus, minus
 
-print(plus(1,2), minus(4,6))
+# print(plus(1,2), minus(4,6))
+
+#List in python 2021/08/15
+import requests
+
+indeed_result = requests.get ("https://kr.indeed.com/jobs?q=python&limit=50")
+
+print(indeed_result.text)
+#html 가져오기 끝
