51c5ca58a1d74bc32058365b38a3b628c12c365b
skinispibu <Kimhagoong@gmail.com>
2021-08-20 23:46:08 +0900

    company_anchor

--- main.py
+++ main.py
@@ -176,27 +176,39 @@
 #     print(result.status_code)
 
 #List in python 2021/08/19
-import requests
-from bs4 import BeautifulSoup
+# import requests
+# from bs4 import BeautifulSoup
 
-LIMIT = 50
-URL = f "https://www.indeed.com/jobs?as_and=python&limit={LIMIT}"
+# LIMIT = 50
+# URL = f "https://www.indeed.com/jobs?as_and=python&limit={LIMIT}"
 
-def exract_indeed_pages():
- result = requests.get(URL)
+# def exract_indeed_pages():
+#  result = requests.get(URL)
 
- soup = BeautifulSoup(result.text, "htmlparser")
+#  soup = BeautifulSoup(result.text, "htmlparser")
 
-  pagination = soup.find("div", {"class":"pagination"})
+#   pagination = soup.find("div", {"class":"pagination"})
 
-  links = pagination.find_all('a')
-  pages = []
-  for link in links[:-1]:
-    pages.append(int(link.string))
+#   links = pagination.find_all('a')
+#   pages = []
+#   for link in links[:-1]:
+#     pages.append(int(link.string))
 
-  max_page = pages[-1]
-  return max_page
+#   max_page = pages[-1]
+#   return max_page
 
+# def extract_indeed_jobs(last_page):
+#   jobs = []
+#   # for page in range(last_page):
+#   result = requests.get(f "{URL}&start={0*LIMIT}") 
+#    soup = BeautifulSoup(result.text, "htmlparser")
+#   results = soup.find_all("div", {"class":"jobsearch-SerpJobCard"})
+#   for result in results:
+#     title = reault.find_all("div", {"class":"title"}).find("a")["title"]
+#     print(title)
+#   return jobs
+
+#List in python 2021/08/20
 def extract_indeed_jobs(last_page):
   jobs = []
   # for page in range(last_page):
@@ -205,8 +217,12 @@ def extract_indeed_jobs(last_page):
   results = soup.find_all("div", {"class":"jobsearch-SerpJobCard"})
   for result in results:
     title = reault.find_all("div", {"class":"title"}).find("a")["title"]
-    print(title)
-  return jobs
-
-
-
+    company = result.find("span",{"class": "company"})
+    company_anchor = company.find("a")
+    if company_anchor is not None:
+      company = str(company_anchor.string)
+    else:
+      company = str(company.string)
+    company = company.strip()
+    print(title, company)  
+  return jobs 
