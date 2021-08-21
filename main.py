f567827a261c4f3cba311533a38a8913f2396fa2
skinispibu <Kimhagoong@gmail.com>
2021-08-21 23:44:31 +0900

    extract_jobs

--- main.py
+++ main.py
@@ -209,14 +209,27 @@
 #   return jobs
 
 #List in python 2021/08/20
-def extract_indeed_jobs(last_page):
-  jobs = []
-  # for page in range(last_page):
-  result = requests.get(f "{URL}&start={0*LIMIT}") 
-   soup = BeautifulSoup(result.text, "htmlparser")
-  results = soup.find_all("div", {"class":"jobsearch-SerpJobCard"})
-  for result in results:
-    title = reault.find_all("div", {"class":"title"}).find("a")["title"]
+# def extract_indeed_jobs(last_page):
+#   jobs = []
+#   # for page in range(last_page):
+#   result = requests.get(f "{URL}&start={0*LIMIT}") 
+#    soup = BeautifulSoup(result.text, "htmlparser")
+#   results = soup.find_all("div", {"class":"jobsearch-SerpJobCard"})
+#   for result in results:
+#     title = reault.find_all("div", {"class":"title"}).find("a")["title"]
+#     company = result.find("span",{"class": "company"})
+#     company_anchor = company.find("a")
+#     if company_anchor is not None:
+#       company = str(company_anchor.string)
+#     else:
+#       company = str(company.string)
+#     company = company.strip()
+#     print(title, company)  
+#   return jobs 
+
+#List in python 2021/08/21
+def extract_job(html):
+  title = reault.find_all("div", {"class":"title"}).find("a")["title"]
     company = result.find("span",{"class": "company"})
     company_anchor = company.find("a")
     if company_anchor is not None:
@@ -224,5 +237,24 @@ def extract_indeed_jobs(last_page):
     else:
       company = str(company.string)
     company = company.strip()
-    print(title, company)  
+    location = html.find("div", {"class": "recJobLoc"})["date-rc-loc"]
+    job_id = html["date-jk"]
+    return {
+      "title": title,
+      'company': company,
+      'location': location,
+      "link": f "https://www.indeed.com/viewjob?jk={job_id}"
+    }
+
+def extract_indeed_jobs(last_page):
+  jobs = []
+  for page in range(last_page):
+    print(f "Scrapping page {page")
+    result = requests.get(f "{URL}&start={page*LIMIT}") 
+    soup = BeautifulSoup(result.text, "htmlparser")
+    results = soup.find_all("div", {"class":"jobsearch-SerpJobCard"})
+  for result in results:
+    job = extract_job(result)
+    jobs.append(job)
   return jobs 
+
